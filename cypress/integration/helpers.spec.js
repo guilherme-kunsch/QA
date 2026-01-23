09d48cc0297b09eb77df69be28b0e6798898909f
guilherme-kunsch <guilherme.kunsch@outlook.com>
2026-01-22 21:20:25 -0300

    finalizando com invoke

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -29,4 +29,34 @@ describe('Helpers', () => {
             console.log('Encontrei o segundo botão!')
         })
     })
+
+    it.only('Its', () => {
+        const obj = {
+            nome: 'User',
+            idade: '23'
+        }
+
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal', 'User')
+
+        const obj2 = {
+            nome: 'User',
+            idade: '23',
+            endereco: {
+                rua: 'dos bobos'
+            }
+        }
+        cy.wrap(obj2).its('endereco').its('rua').should('contain', 'bobos')
+        cy.wrap(obj2).its('endereco.rua').should('contain', 'bobos')
+
+    })
+
+    it.only('Invoke', () => {
+        const getValue = () => 1;
+        const soma = (a, b) => a + b;
+
+        cy.wrap({fn: getValue}).invoke('fn').should('be.equal', 1)
+        cy.wrap({fn: soma}).invoke('fn', 2, 5).should('be.equal', 7)
+
+    })
 })
