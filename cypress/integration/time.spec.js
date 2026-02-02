5dc582ef0441d4685cbb97d5ecc9846764437141
guilherme-kunsch <guilherme.kunsch@outlook.com>
2026-02-02 07:51:36 -0300

    cypress tick

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -18,4 +18,21 @@ describe('Dinamic tests', () => {
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain', '31/12/1969')
     })
+
+    it.only('Goes to the future', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gt', 1770029187587)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+
+        cy.wait(1000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 5000)
+    })
 })
